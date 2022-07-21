709bbaec061623c3de47d886e3dce75174f6de1a
MaxVidom <MaxVidomenko@gmail.com>
2022-07-21 08:36:54 +0300

    Compleate task with LocalStorage

---
+++ src/components/AddContactForm/AddContactForm.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class addContactsForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = evt => {
+    this.setState({
+      [evt.currentTarget.name]: evt.currentTarget.value,
+    });
+  };
+
+  handleSubmitForm = evt => {
+    const { name, number } = this.state;
+    evt.preventDefault();
+    this.props.onSubmit(name, number);
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form action="">
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={this.handleInputChange}
+        />
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={this.handleInputChange}
+        />
+        <button type="submit" onClick={this.handleSubmitForm}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+export default addContactsForm;
---
+++ src/components/App.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import AddContactForm from './AddContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevProps.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = (name, number) => {
+    const { contacts } = this.state;
+    const normalizedName = name.toLowerCase();
+    if (
+      contacts.some(contact => contact.name.toLowerCase() === normalizedName)
+    ) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  changeFilter = evt => {
+    const { value } = evt.currentTarget;
+    this.setState({
+      filter: value,
+    });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const visibleContacts = this.getVisibleContacts();
+    const { filter } = this.state;
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <AddContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </>
+    );
+  }
+}
+
+export default App;
